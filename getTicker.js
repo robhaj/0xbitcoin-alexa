5ddaa698db695304e027265be59db2a0c04c81a3
Robby Hajek <hajek.rob@gmail.com>
2018-06-14 10:28:45 -0600

    add cmc req wrapper

---
+++ getTicker.js
@@ -0,0 +1,7 @@
+  'use strict';
+  const rp = require('request-promise');
+
+  module.exports = async() => {
+    let res = await rp.get('https://api.coinmarketcap.com/v2/ticker/2837/');
+    return JSON.parse(res).data.quotes.USD;
+  };
